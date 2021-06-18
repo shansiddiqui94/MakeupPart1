1424005bd3e81efc778bf3d74d99048cdfcf56bd
Shan Siddiqui <shansiddiqui@Shans-Air.lan>
2021-06-18 17:36:05 -0400

    Final

---
+++ SQL.js
@@ -0,0 +1,19 @@
+//SQL QUERY
+ 
+//3:  
+// SELECT ord_no, ord_date, purch_amt
+// FROM orders
+// WHERE salesman_id=5001;
+
+//4 
+// SELECT salesman.name, customer.cust_name, customer.city
+// FROM salesman
+// INNER JOIN customer
+// ON salesman.city=customer.city;
+
+//5
+//SELECT Winners 
+//FROM Physiology AND YEAR < 1971
+//SELECT FROM  Winners 
+//WHERE Peace AND year >= 1974
+
--- test1.js
+++ test1.js
@@ -12,3 +12,20 @@
 //     console.log(cars())
 
 //____________________________________________________________
+
+//2. Algo:
+
+//  function merge (nums1, m, nums2, n) {
+//     let i = m - 1, j = n - 1;
+//     let end = m + n - 1;
+//     while (i >= 0 && j >= 0) {
+//         if (nums1[i] < nums2[j]) {
+//            return nums1[end--] = nums2[j--];
+//         } else {
+//             nums1[end--] = nums1[i--];
+//         }
+//     }
+//     while (j >= 0) {
+//         nums1[end--] = nums2[j--];
+//     }
+// };
