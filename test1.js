9899903547fbe80fe282d638a756a93093efb9d2
Shan Siddiqui <shansiddiqui@Shans-Air.lan>
2021-06-18 16:53:52 -0400

    testing

---
+++ test1.js
@@ -0,0 +1,14 @@
+//1. Data Structures and Algorithms:
+
+// function cars(){
+
+//     let wordCounts = { };
+//     let words = text.split(" ");
+    
+//     for(let i = 0; i < words.length; i++)
+//         wordCounts["_" + words[i]] = (wordCounts["_" + words[i]] || 0) + 1;
+//         return wordCounts
+//     }
+//     console.log(cars())
+
+//____________________________________________________________
